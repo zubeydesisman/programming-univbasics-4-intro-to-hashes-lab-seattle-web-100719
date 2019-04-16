222717eff8ef86b288ef7f6a74c52e594fc01876
maxwellbenton <maxwellbenton@gmail.com>
2019-04-16 19:36:20 -0400

    updates solution

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,30 +1,28 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
-
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  { "Programming" => "is great!" }
 end
 
 def actor
-  # return a hash with a key of :name and a corresponding value of 'Dwayne The Rock Johnson'
+  {name: 'Dwayne The Rock Johnson'}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to a random integer or float
+  { id: rand }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  {key => value}
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
 end
